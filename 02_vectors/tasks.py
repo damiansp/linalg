46c4b5373d897e89b81ca7cdf5e1efdba15a9820
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2021-04-07 07:40:45 -0700

    tasks +add, neg

--- 02_vectors/tasks.py
+++ 02_vectors/tasks.py
@@ -23,16 +23,13 @@ class Vec:
     def set_item(self, key, val):
         self.f[key] = val
 
-    def get_item(self, key):
-        return self.f.get(key, None)
+    def get_item(self, key, default=None):
+        return self.f.get(key, default)
 
     def scalar_mul(self, alpha):
         prod = Vec(self.D, {k: alpha*v for k, v in self.f.items()})
         return prod
                    
-
-v = Vec({'A', 'B', 'C'}, {'A': 1, 'B': 2, 'C': 3})
-
 
 def zero_vec(D, sparse=False):
     assert type(D) is set, 'D must be a set'
@@ -41,10 +38,23 @@ def zero_vec(D, sparse=False):
     return Vec(D, {i: 0 for i in D})
 
 
+def add(u, v):
+    keys = u.D
+    dct = {k: u.f.get(k, 0) + v.f.get(k, 0) for k in keys}
+    return Vec(keys, dct)
+
+
+def neg(v):
+    return v.scalar_mul(-1)
+
 
 # Test
+v = Vec({'A', 'B', 'C'}, {'A': 1, 'B': 2, 'C': 3})
 b = v.scalar_mul(3)
 for key, val in b.f.items():
     print(f'{key} -> {val}')
 
-
+u = Vec(v.D, {'A': 5., 'C': 10.})
+x = add(u, v)
+print(x.f)
+print(neg(x).f)
